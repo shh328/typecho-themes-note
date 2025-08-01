504127b7131c8be39d549086a2f71098bc442d0f
shh328 <shh@shinote.net>
2025-08-01 10:39:50 +0800

    Update index.php

--- index.php
+++ index.php
@@ -15,20 +15,19 @@ $this->need('header.php');
 <div class="col-mb-12 col-8" id="main" role="main">
     <?php while ($this->next()): ?>
         <article class="post" itemscope itemtype="http://schema.org/BlogPosting">
-            <h2 class="post-title" itemprop="name headline">
-                <a itemprop="url"
-                   href="<?php $this->permalink() ?>"><?php $this->title() ?></a>
-            </h2>
-            <ul class="post-meta">
-                <li><?php _e('时间: '); ?>
-                    <time datetime="<?php $this->date('c'); ?>" itemprop="datePublished"><?php $this->date(); ?></time>
-                </li>
-                <li><?php _e('分类: '); ?><?php $this->category(','); ?></li>
-                <li itemprop="interactionCount">
-                    <a itemprop="discussionUrl"
-                       href="<?php $this->permalink() ?>#comments"><?php $this->commentsNum('评论', '1 条评论', '%d 条评论'); ?></a>
-                </li>
-            </ul>
+            <div class="post-header">
+                <h2 class="post-title-left" itemprop="name headline">
+                    <a itemprop="url" href="<?php $this->permalink() ?>"><?php $this->title() ?></a>
+                </h2>
+                <ul class="post-meta-right">
+                    <li class="post-meta-item">
+                        <time datetime="<?php $this->date('c'); ?>" itemprop="datePublished"><?php $this->date(); ?></time>
+                    </li>
+                    <li class="post-meta-item" itemprop="interactionCount">
+                        <a itemprop="discussionUrl" href="<?php $this->permalink() ?>#comments"><?php $this->commentsNum('评论', '1 条评论', '%d 条评论'); ?></a>
+                    </li>
+                </ul>
+            </div>
         </article>
     <?php endwhile; ?>
 
