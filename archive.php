083c8aa63c024e04c43c4f734445613de09c5b49
shh328 <shh@shinote.net>
2025-08-01 10:39:16 +0800

    Update archive.php

--- archive.php
+++ archive.php
@@ -10,21 +10,21 @@
         ], '', ''); ?></h3>
     <?php if ($this->have()): ?>
         <?php while ($this->next()): ?>
-            <article class="post" itemscope itemtype="http://schema.org/BlogPosting">
-                <h2 class="post-title" itemprop="name headline"><a itemprop="url"
-                                                                   href="<?php $this->permalink() ?>"><?php $this->title() ?></a>
+        <article class="post" itemscope itemtype="http://schema.org/BlogPosting">
+            <div class="post-header">
+                <h2 class="post-title-left" itemprop="name headline">
+                    <a itemprop="url" href="<?php $this->permalink() ?>"><?php $this->title() ?></a>
                 </h2>
-                <ul class="post-meta">
-                    <li><?php _e('时间: '); ?>
-                        <time datetime="<?php $this->date('c'); ?>"
-                              itemprop="datePublished"><?php $this->date(); ?></time>
+                <ul class="post-meta-right">
+                    <li class="post-meta-item">
+                        <time datetime="<?php $this->date('c'); ?>" itemprop="datePublished"><?php $this->date(); ?></time>
                     </li>
-                    <li><?php _e('分类: '); ?><?php $this->category(','); ?></li>
-                    <li itemprop="interactionCount"><a
-                            href="<?php $this->permalink() ?>#comments"><?php $this->commentsNum('评论', '1 条评论', '%d 条评论'); ?></a>
+                    <li class="post-meta-item" itemprop="interactionCount">
+                        <a itemprop="discussionUrl" href="<?php $this->permalink() ?>#comments"><?php $this->commentsNum('评论', '1 条评论', '%d 条评论'); ?></a>
                     </li>
                 </ul>
-            </article>
+            </div>
+        </article>
         <?php endwhile; ?>
     <?php else: ?>
         <article class="post">
